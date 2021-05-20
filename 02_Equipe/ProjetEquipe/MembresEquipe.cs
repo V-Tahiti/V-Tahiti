ff73f87f5d6609c0e1da6812158f4f20a6e9da07
V-Tahiti <34101025+V-Tahiti@users.noreply.github.com>
2021-05-20 22:54:54 +0200

    Add files via upload

---
+++ 02_Equipe/ProjetEquipe/MembresEquipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetEquipe
+{
+    class MembresEquipe
+    {
+        private string Nom;
+        private string Prénom;
+        private string Fonction;
+
+        public MembresEquipe(string Nom, string Prénom, string Fonction)
+        {
+            this.Nom = Nom;
+            this.Prénom = Prénom;
+            this.Fonction = Fonction;
+        }
+
+        public void Membre()
+        {
+            Console.WriteLine("Bonjour je suis {0} {1} et j'occupe la fonction : {2}",Nom,Prénom,Fonction);
+        }
+
+    }
+
+}
---
+++ 02_Equipe/ProjetEquipe/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProjetEquipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MembresEquipe Vaitua = new MembresEquipe("Vaitua", "MAKE", "Capitaine");
+            Vaitua.Membre();
+
+            MembresEquipe Alexandra = new MembresEquipe("Alexandra", "Muller", "Défense Gauche");
+            Alexandra.Membre();
+            
+            MembresEquipe Laura = new MembresEquipe("Laura", "Wagner", "Défense Droit");
+            Laura.Membre();
+            Console.ReadKey();
+
+
+            /*enum NomEquipe
+              {
+             Pirahnnas,
+             BullDogs,
+             Chihuahua,
+             Eagles,
+              }
+             enum ListeJoueur
+             {
+             Fabien,
+             Cyril,
+             Agathe,
+             Cécile,
+             Amandine,
+             Sylive,
+             }*/
+
+        }
+    }
+}
