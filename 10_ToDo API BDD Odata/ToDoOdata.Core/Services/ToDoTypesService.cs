0f1395289fb050797061b5cd1c000e5bb687e417
V-Tahiti <34101025+V-Tahiti@users.noreply.github.com>
2021-05-23 19:57:36 +0200

    Update ToDoTypesService.cs

--- 10_ToDo API BDD Odata/ToDoOdata.Core/Services/ToDoTypesService.cs
+++ 10_ToDo API BDD Odata/ToDoOdata.Core/Services/ToDoTypesService.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ToDoOdata.Core.Context;
 using ToDoOdata.Models;
 
 namespace ToDoOdata.Core.Services
 {
-    public class ToDoTypesService<TContext> : GenericService<TContext, ToDoType> where TContext : DbContext
-    {
-        public ToDoTypesService(TContext context) : base(context)
+    public class ToDoTypesService
         {
+        private readonly ToDoOdataContext context;
 
+        public ToDoTypesService(ToDoOdataContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<ToDoType> GetAllToDoTypes()
+        {
+            return context.ToDoTypes;
         }
     }
 }
+
