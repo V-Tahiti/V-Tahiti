6e425748e5706229cf9d8f6e88d85bad21b187b7
V-Tahiti <34101025+V-Tahiti@users.noreply.github.com>
2021-05-23 21:12:57 +0200

    Update SeedData.cs

--- 10_ToDo API BDD Odata/ToDoOdata.Core/Context/SeedData.cs
+++ 10_ToDo API BDD Odata/ToDoOdata.Core/Context/SeedData.cs
@@ -6,17 +6,20 @@ using ToDoOdata.Core.Context;
 using ToDoOdata.Models;
 
 namespace ToDoOdata.Core
-{
+{//alimentation de la BDD Virtuelle
     public class SeedData
     {
         public static void Initialize(ToDoOdataContext context)
         {
+            //Si rien n'existe dans les propriétés du context(BDD), il faut ajouter avec la méthode "Initialize" les objets
+            //ToDo et ToDoType avec les références relationnelles de "Foreign Key" définies dans le model de "ToDo"
             if (!context.ToDoes.Any() && !context.ToDoTypes.Any())
             {
                 context.ToDoes.AddRange(
                     new ToDo
                     {
                         Description = "A faire1",
+                        //relation de "Foreign Key" pour ToDoType
                         ToDoType_Id = 1
                     },
                     new ToDo
