b7dc13952bee9952858caec3d89743e448afda46
V-Tahiti <34101025+V-Tahiti@users.noreply.github.com>
2021-05-23 20:53:16 +0200

    Update ToDoesController.cs

--- 10_ToDo API BDD Odata/ToDoOdata.OData/Controllers/ToDoesController.cs
+++ 10_ToDo API BDD Odata/ToDoOdata.OData/Controllers/ToDoesController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Mvc;
+using ToDoOdata.Core;
 using ToDoOdata.Core.Context;
 using ToDoOdata.Core.Services;
 using ToDoOdata.Models;
@@ -9,15 +11,36 @@ using ToDoOdata.OData.Controllers.Generic;
 
 namespace ToDoOdata.OData.Controllers
 {
-    public class ToDoesController : GenericController<ToDoOdataContext, ToDo>
+
+    public class ToDoesController : ODataController
+
     {
-        public ToDoesController(ToDoOdataContext context) : base(new ToDoesService<ToDoOdataContext>(context)) { }
+        private readonly ToDoesService service;
+
+
+        public ToDoesController(ToDoOdataContext context)
+        {
+            this.service = new ToDoesService(context);
+            SeedData.Initialize(context);
+        }
 
         [HttpGet]
-        [EnableQuery()]
-        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDoes()
+        [EnableQuery]
+        public IEnumerable<ToDo> GetToDoes()
         {
-            return await base.Gets();
+            return service.GetAllToDoes();
+        }
+
+        [HttpGet ("{key}")]
+        [EnableQuery]
+        public async Task<ActionResult<ToDo>> GetTodo(int key)
+        {
+            var todo = await service.GetToDoById(key);
+            if (todo ==null)
+            {
+                return NotFound();
+            }
+            return todo;
         }
     }
 }
